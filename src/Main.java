546ec8a9d172a222f79d67c91ac417af036989ec
Babbonat <44708155+Babbonat@users.noreply.github.com>
2024-01-25 21:58:41 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,3 @@
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args) {
